1aa6c663f99ceedf152783cea7b25fddcdd88036
zhangda <zhangda1945@163.com>
2019-08-28 18:02:58 +0800

    项目初始化+顶部通栏+左侧导航栏

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/index'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [{
+          path: '/',
+          redirect: {
+              name: 'index'
+          }
+      },
+      {
+          path: '/index',
+          name: 'index',
+          component: Index
+      },
+  ]
+})
