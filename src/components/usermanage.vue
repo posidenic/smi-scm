cf33db9703b3336a03841225d53d8ab162c5f6ff
zhangda <zhangda1945@163.com>
2019-08-28 22:44:02 +0800

    顶栏侧栏抽离出成为公共区域，补传四个配置文件

---
+++ src/components/usermanage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="panel panel-default">
+        <div class="panel-heading">用户管理</div>
+        <div class="panel-body">
+          <div class="row">用户管理页面具体内容</div>
+          <!-- /.row (nested) -->
+        </div>
+        <!-- /.panel-body -->
+      </div>
+      <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+  </div>
+  <!-- /.row -->
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style>
+.panel-default > .panel-heading {
+  color: #333;
+  background-color: #fff;
+  border-color: #ddd;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
+import UserManage from '@/components/usermanage'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: [{
-          path: '/',
-          redirect: {
-              name: 'index'
-          }
-      },
-      {
-          path: '/index',
-          name: 'index',
-          component: Index
-      },
-  ]
+    mode: 'history',
+    routes: [{
+        path: '/',
+        redirect: {
+            name: 'index'
+        }
+    },
+    {
+        path: '/index',
+        name: 'index',
+        component: Index
+    },
+    {
+        path: '/usermanage',
+        name: 'usermanage',
+        component: UserManage
+    },
+    ]
 })
